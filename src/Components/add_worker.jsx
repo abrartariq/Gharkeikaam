6af5d46d9003279accd605a197e2a5ad4f195d64
ZohaAshfaq <ashfaq.zoha@gmail.com>
2019-05-16 04:37:41 +0500

    Error_Checks_added

--- src/Components/add_worker.jsx
+++ src/Components/add_worker.jsx
@@ -1,56 +1,249 @@
-import React, { Component } from 'react';
-import './add_worker.css'
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
+import "./add_worker.css";
+
+function isAlpha(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (
+      //!(code > 47 && code < 58) && //  (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123) // lower alpha (a-z)
+      // Space
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isAlphaNumeric(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (
+      !(code > 47 && code < 58) && //  (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123) // lower alpha (a-z)
+      // Space
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isNumeric(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (!(code > 47 && code < 58)) {
+      //  (0-9)
+      return false;
+    }
+  }
+  return true;
+}
+
+const INITIAL_STATE = {
+  firstname: "",
+  lastname: "",
+  cnic: "",
+  service: "",
+  phonenumber: "",
+  gender: "",
+  address: ""
+};
 
 class AddWorker extends Component {
-    state = {  }
-    render() { 
-        return (
-            <div className="addworker-form">
-            <form action="" method="post">
-                <h2 color = "#242487"> Add Worker</h2><br></br>
-                <div className="form-group">
-                <div className="file-field">
-                <div className="z-depth-1-half mb-4">
-                 <img src= {require('./worker.png')} className="img-fluid-worker" alt="example placeholder"/><br></br>
-                </div>
-                <div className="d-flex justify-content-center">
-                <div className="btn btn-mdb-color btn-rounded float-left">
-                <input type="file"/>
-            </div>
-            </div>
-            </div>
-            </div>
-                <div className="form-group">
-                <input type="fullname" className="form-control" name="fullname" placeholder="Full Name" id="name" required="required"/><br></br>
-                <div className="form-group">
-                <select className="form-control" id="exampleFormControlSelect1">
-                <option>Select Gender...</option>
-                 <option>Male</option>
-                 <option>Female</option>
-                 </select><br></br>
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
 
-           <input type="text" className="form-control" name="cnic" placeholder="CNIC" required="required"/>
-                    <div className="form-group"><br></br>
-                    <input type="text" className="form-control" name="address" placeholder="Address" required="required"/>
-                    <div className="form-group"><br></br>
-                    <input type="text" className="form-control" name="contact" placeholder="Contact No." required="required"/>
-                    <div className="form-group"><br></br>
-                    <select className="form-control" id="exampleFormControlSelect1">
-                    <option>Select Service...</option>
-                 <option>Plumber</option>
-                 <option>Gardener</option>
-                 <option>Electrician</option>
-                 <option>Cleaner</option>
-                 </select><br></br>
-                <button type="submit" className="button-worker">Save</button>
-                </div> 
-                </div>
-                </div>
-                </div>
-                </div>
-                </form>
-                </div>
-         );
+  onChange = event => {
+    // console.log(event.target.name);
+    if (event.target.name == "fullname") {
+      let full_name = event.target.value.split(" ");
+      let fname = full_name.shift();
+      let lname = full_name.join(" ");
+      this.setState({ firstname: fname, lastname: lname });
+    } else {
+      this.setState({ [event.target.name]: event.target.value });
     }
+  };
+
+  onSubmit = event => {
+    const {
+      firstname,
+      lastname,
+      cnic,
+      service,
+      phonenumber,
+      gender,
+      address
+    } = this.state;
+    event.preventDefault();
+
+    console.log(
+      firstname,
+      lastname,
+      cnic,
+      service,
+      phonenumber,
+      gender,
+      address
+    );
+    if (
+      firstname == "" ||
+      cnic == "" ||
+      service == "" ||
+      phonenumber == "" ||
+      gender == "" ||
+      address == ""
+    ) {
+      window.alert("All Fields Are Mandadory, Please Fill All Fields");
+      // event.preventDefault()
+    } else if (!isAlpha(firstname) || !isAlpha(lastname)) {
+      console.log("NOT ALPH");
+      window.alert("Name can only contain Letters. Please Try Again");
+      // event.preventDefault();
+    } else if (!isNumeric(cnic)) {
+      window.alert("CNIC can only contain Numbers. Please Try Again");
+      // event.preventDefault();
+    } else if (!isNumeric(phonenumber)) {
+      window.alert("PhoneNumber can only contain Number. Please Try Again");
+      // event.preventDefault();
+    } else if (service == "Select Service...") {
+      window.alert("Please Choose A Service");
+      // event.preventDefault();
+    } else if (gender == "Select Gender...") {
+      window.alert("Please Choose A Gender");
+      // event.preventDefault();
+    } else {
+      console.log("About to submit");
+      this.props.firebase.doSaveNewWorker(this.state).then(() => {
+        console.log("DONESSS");
+      });
+
+      // this.props.history.push({
+      //   pathname: ROUTES.CONFIRMATION,
+      // });
+
+      // this.setState({ ...INITIAL_STATE });
+      // event.preventDefault();
+    }
+    // event.preventDefault()
+  };
+
+  render() {
+    return (
+      <div className="addworker-form">
+        <form onSubmit={this.onSubmit}>
+          <h2 color="#242487"> Add Worker</h2>
+          <br />
+          <div className="form-group">
+            <div className="file-field">
+              <div className="z-depth-1-half mb-4">
+                <img
+                  src={require("./worker.png")}
+                  className="img-fluid-worker"
+                  alt="example placeholder"
+                />
+                <br />
+              </div>
+              <div className="d-flex justify-content-center">
+                <div className="btn btn-mdb-color btn-rounded float-left">
+                  <input type="file" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="form-group">
+            <input
+              type="fullname"
+              className="form-control"
+              name="fullname"
+              id="fullname"
+              onChange={this.onChange}
+              placeholder="Full Name"
+              required="required"
+            />
+            <br />
+            <div className="form-group">
+              <select
+                className="form-control"
+                id="gender"
+                name="gender"
+                onChange={this.onChange}
+              >
+                <option>Select Gender...</option>
+                <option>Male</option>
+                <option>Female</option>
+              </select>
+              <br />
+
+              <input
+                type="text"
+                className="form-control"
+                name="cnic"
+                id="cnic"
+                onChange={this.onChange}
+                placeholder="CNIC"
+                required="required"
+              />
+              <div className="form-group">
+                <br />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="address"
+                  id="address"
+                  placeholder="Address"
+                  onChange={this.onChange}
+                  required="required"
+                />
+                <div className="form-group">
+                  <br />
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="phonenumber"
+                    id="contact"
+                    placeholder="Contact No."
+                    required="required"
+                    onChange={this.onChange}
+                  />
+                  <div className="form-group">
+                    <br />
+                    <select
+                      className="form-control"
+                      name="service"
+                      onChange={this.onChange}
+                    >
+                      <option>Select Service...</option>
+                      <option>Plumber</option>
+                      <option>Gardener</option>
+                      <option>Electrician</option>
+                      <option>Cleaner</option>
+                    </select>
+                    <br />
+                    <button type="submit" className="button-worker">
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-export default AddWorker;
+export default withRouter(AddWorker);

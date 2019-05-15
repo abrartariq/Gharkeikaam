7481c1c82a67c84004c5d13cae90fc905ef1b8fa
FShoaib <feryalferry@gmail.com>
2019-05-16 03:44:18 +0500

    Admin_Footer

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header_afterSignIn from '../Components/Header_afterSignIn'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
 import Abouts  from '../Components/about'
@@ -6,11 +7,26 @@ import Abouts  from '../Components/about'
 
 class About extends Component {
     render() {
+      let about
+      if (this.props.history.location.state != undefined){
+
+        about = <div className="Login_Page_App">
+        <Header_afterSignIn email={this.props.history.location.state.detail}/>
+        <Abouts />
+        <Footer />
+        </div>
+      }
+      else {
+        about = <div className="Login_Page_App">        
+          <Header />
+          <Abouts />
+          <Footer />
+          </div>
+      }
+
       return (
       <div className="Login_Page_App">
-          <Header/>
-         <Abouts />
-        <Footer />
+        {about}
         
         </div>
       );
--- src/pages/Add_Worker.js
+++ src/pages/Add_Worker.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-import Header from '../Components/Header'
-import Footer from '../Components/Footer'
+import Header from '../Components/Header_Admin'
+import Footer from '../Components/Footer_admin'
 import Addworker from '../Components/add_worker'
+import { FirebaseContext } from '../Components/Firebase'
 
 
 class Addworker_Worker_Page extends Component {
@@ -11,7 +12,10 @@ class Addworker_Worker_Page extends Component {
       <div className="Addworker_Worker_Page">
 
           <Header />
-            <Addworker />
+          <FirebaseContext.Consumer>
+            {firebase => <Addworker firebase={firebase} />}
+          </FirebaseContext.Consumer>
+
           <Footer />
         
         </div>

982425f65fbfdd5612be1f4fdc3f3f5d1b132e4f
FShoaib <feryalferry@gmail.com>
2019-05-16 03:50:59 +0500

    Contact_Page_Updated

--- src/pages/Contact_Page_App.js
+++ src/pages/Contact_Page_App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header'
+import Header_afterSignIn from '../Components/Header_afterSignIn'
 import Footer from '../Components/Footer'
 import ContactUs from '../Components/contactus'
 
 class Contact_Page_App  extends Component {
   render() {
+
+    let contact
+    if (this.props.history.location.state != undefined){
+
+      contact = <div className="Contact_Page_App">
+      <Header_afterSignIn email={this.props.history.location.state.detail}/>
+      <ContactUs />
+      <Footer />
+      </div>
+    }
+    else {
+      contact = <div className="Contact_Page_App">        
+        <Header />
+        <ContactUs />
+        <Footer />
+        </div>
+    }
+
     return (
-	<div className="Contact_Page_App ">
-		<Header/>
-      { <ContactUs/> }
-      {<Footer />}
-      
+      <div className="Contact_Page_App ">
+        {contact}
       </div>
     );
   }

12ed4c9fc6a792e427e1d998ebb76cb1ee93d403
abubakaraziz <aziz313f@gmail.com>
2019-05-16 16:08:06 +0500

    FINAL_PUSH

---
+++ build/precache-manifest.edc1b6a6447f5f45c58b2d371590351e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb8cf1d9e19ee87af93a22ba3cdcdddc",
+    "url": "/static/media/howwework.cb8cf1d9.png"
+  },
+  {
+    "revision": "5d110e80fdf5b31057f1",
+    "url": "/static/css/main.233a6451.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb1da10445585b6f7a304c8067411f4c",
+    "url": "/static/media/Worker.eb1da104.jpeg"
+  },
+  {
+    "revision": "c00d55ec05f95daaa91b",
+    "url": "/static/js/2.f24fb22d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "842cb8994a97581269db1a95ff05c03e",
+    "url": "/static/media/GKK.842cb899.png"
+  },
+  {
+    "revision": "68a7d5b3c58a40155765199addad255c",
+    "url": "/static/media/worker.68a7d5b3.png"
+  },
+  {
+    "revision": "4eeaa8521296de540c9acf96f865f2d6",
+    "url": "/static/media/services.4eeaa852.png"
+  },
+  {
+    "revision": "5d110e80fdf5b31057f1",
+    "url": "/static/js/main.d7ec957e.chunk.js"
+  },
+  {
+    "revision": "e24946c5f126d4448d132a27102e7f10",
+    "url": "/static/media/about.e24946c5.jpg"
+  },
+  {
+    "revision": "bf1f4b6cfb275cc70abba784d86f94cd",
+    "url": "/static/media/electrical.bf1f4b6c.png"
+  },
+  {
+    "revision": "5deb9451fdbed5c964170e3d7a573f79",
+    "url": "/static/media/plumbing.5deb9451.png"
+  },
+  {
+    "revision": "a034135d8dac7149ed2caeff58631d17",
+    "url": "/static/media/cleaning.a034135d.png"
+  },
+  {
+    "revision": "3b45178e432b3b0d99c3b5275f0166ab",
+    "url": "/static/media/gardening.3b45178e.png"
+  },
+  {
+    "revision": "a31574ca4f586a81bb22817c21207900",
+    "url": "/static/media/Spinner.a31574ca.gif"
+  },
+  {
+    "revision": "c00d55ec05f95daaa91b",
+    "url": "/static/css/2.d2f13df0.chunk.css"
+  },
+  {
+    "revision": "9cd67d73c82836ce08d340b2ad9926ed",
+    "url": "/index.html"
+  }
+];

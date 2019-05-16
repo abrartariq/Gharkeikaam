4fb43bac8c512bc8f66e2bee94a5bd576468a894
abrartariq <abrartariq14@gmail.com>
2019-05-16 05:20:22 +0500

    Waiting_Updated

--- src/Components/Waiting.jsx
+++ src/Components/Waiting.jsx
@@ -1,52 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import * as ROUTES from "../constants/routes";
-import { withRouter } from 'react-router-dom';
-import './Waiting.css';
+import { withRouter } from "react-router-dom";
+import "./Waiting.css";
 
 class Waiting extends Component {
-	state = {  }
+  state = {};
 
-	constructor(props) {
-		super(props);
-		console.log('Email is',this.props.email)
-		console.log('requestid is',this.props.requestid)
+  constructor(props) {
+    super(props);
+    console.log("Email is", this.props.email);
+    console.log("requestid is", this.props.requestid);
+  }
 
-	}
+  changetoprocessing2 = () => {
+    const self = this;
+    self.props.history.push({
+      pathname: ROUTES.PROCESSING_REQUEST2,
+      state: { detail: this.props.email, id: this.props.requestid }
+    });
+  };
 
-	
-	changetoprocessing2=()=>{
-		const self=this;
-		self.props.history.push({
-			pathname: ROUTES.PROCESSING_REQUEST2,
-			state: { detail: this.props.email, id:this.props.requestid }
-			
-		})
-	}
+  changetorejected = () => {
+    const self = this;
+    self.props.history.push({
+      pathname: ROUTES.REJECTION,
+      state: { detail: this.props.email }
+    });
+  };
 
+  componentWillMount() {
+    let database = this.props.firebase.getdatabase();
+    const self = this;
+    database
+      .collection("CurrentRequests")
+      .doc(this.props.requestid)
+      .onSnapshot(function(snapshot) {
+        const datas = snapshot.data();
+        if (datas["status"] == "inprogress") {
+          self.changetoprocessing2();
+          console.log("here");
+        } else if (datas["status"] == "rejected") {
+          self.changetorejected();
+          console.log("From in Progress to rejected");
+        }
+      });
+  }
 
-	componentWillMount(){
-		let database=this.props.firebase.getdatabase()
-		const self=this
-		database.collection("CurrentRequests").doc(this.props.requestid).onSnapshot(function(snapshot){
-		
-		const datas=snapshot.data()
-		if (datas['status']=="inprogress"){
-			self.changetoprocessing2()
-			console.log('here')
-		}
-})}
-
-
-	render() { 
-		return ( 
-			<div className="card bg-light text-dark abc">
-                <img src={require('./Spinner.gif')} className="img-fluid z"></img>
-                <p className="text-center font-weight-bold">
-                Request Status: Waiting for Approval
-                </p>
-            </div>
-		);
-	}
+  render() {
+    return (
+      <div className="card bg-light text-dark c">
+        <img src={require("./Spinner.gif")} className="img-fluid z" />
+        <p className="text-center font-weight-bold">
+          Request Status: Waiting for Approval
+        </p>
+      </div>
+    );
+  }
 }
- 
+
 export default withRouter(Waiting);
--- src/Components/view_worker.jsx
+++ src/Components/view_worker.jsx
@@ -1,45 +1,73 @@
-import React, { Component } from 'react';
-import './view_worker.css';
+import React, { Component } from "react";
+import "./view_worker.css";
 
 class ViewWorker extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className="viewworker-form">
-            <form action="" method="post">
+  state = {};
+  render() {
+    return (
+      <div className="viewworker-form">
+        <form action="" method="post">
+          <div className="form-group">
+            <img
+              src={require("./worker.png")}
+              className="img-fluid-worker"
+              alt="example placeholder"
+            />
+            <br />
+            <h2>Abdullah Arif</h2>
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="form-group">
+              <p>
+                <b>Name:</b>{" "}
+              </p>
+              <div className="form-group">
+                <p>
+                  <b>ID:</b>{" "}
+                </p>
                 <div className="form-group">
-					<img src= {require('./worker.png')} className="img-fluid-worker" alt="example placeholder"/><br></br>
-					<h2>Abdullah Arif</h2><br></br><br></br><br></br><br></br>
-						<div className="form-group">
-		                <p><b>Name:</b> </p>
-			                <div className="form-group">
-			                <p><b>ID:</b> </p>
-				                <div className="form-group">
-				                <p><b>Sex:</b> </p>
-					                <div className="form-group">
-					                <p><b>Address:</b> </p>
-						                <div className="form-group">
-						                <p><b>Contact Number:</b> </p>
-								                <div className="form-group">
-									                <p><b>CNIC:</b> </p>
-								                <div className="form-group">
-								                <p><b>Service:</b> </p>
-								                <div className="form-group">
-									                <p><b>Average Rating:</b> </p><br></br>
-									                <button type="submit" className="button-workerview">Edit Details</button>
-								                </div> 
-								                </div>
-								                </div>
-						                </div>
-					                </div> 
-				                </div>
-			                </div>
-		                </div> 
-                </div>     
-            </form>
+                  <p>
+                    <b>Sex:</b>{" "}
+                  </p>
+                  <div className="form-group">
+                    <p>
+                      <b>Address:</b>{" "}
+                    </p>
+                    <div className="form-group">
+                      <p>
+                        <b>Contact Number:</b>{" "}
+                      </p>
+                      <div className="form-group">
+                        <p>
+                          <b>CNIC:</b>{" "}
+                        </p>
+                        <div className="form-group">
+                          <p>
+                            <b>Service:</b>{" "}
+                          </p>
+                          <div className="form-group">
+                            <p>
+                              <b>Average Rating:</b>{" "}
+                            </p>
+                            <br />
+                            <button type="submit" className="button-workerview">
+                              Edit Details
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-         );
-    }
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default ViewWorker;
